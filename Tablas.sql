e5c6055c2fa54352a68bf96acf9a386419e7d276
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-07-11 18:33:15 -0300

    Update Tablas.sql

--- Tablas.sql
+++ Tablas.sql
@@ -17,22 +17,16 @@ CREATE TABLE nota
 ( 
 num_funcionario serial not null,
 ci_alumno smallint references persona (ci) constraint nota_ci_alumno_fk_persona not null,
-cod_materia integer not null,
-primary key fechah_hora DATETIME YEAR TO MINUTE NOT NULL CONSTRAINT Historial_fecha,
-
-baja boolean NOT NULL constraint alumno_baja
-);
-
-CREATE TABLE nota
-( 
-num_funcionario serial not null,
-ci_alumno smallint references persona (ci) constraint nota_ci_alumno_fk_persona not null,
 cod_materia int not null,
 fechah_hora_nota  datetime year to minute primary key  NOT NULL CONSTRAINT Historial_fecha,
 nota int check (nota > 0 and nota < 13),
 tipo varchar(30) check (tipo in ('Oral','Escrito','Proyecto')) not null , 
 baja boolean NOT NULL constraint nota_baja
 );
-pepe 
 
-
+CREATE TABLE lista (
+cod_lista serial primary key constraint lista_primaria not null,
+cod_grupo integer references grupo (cod_grupo) constraint lista_cod_grupo_fk_grupo not null,
+ci_docente integer references persona (ci) constraint lista_ci_docente_fk_persona,
+baja boolean NOT NULL constraint lista_baja
+)
