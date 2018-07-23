22ea7e6700d04212e926d01f69cbcc260a845ea7
ggomez97 <gabriel.mendaro97@gmail.com>
2018-07-23 17:54:48 -0300

    dsa

--- script_backup.sh
+++ script_backup.sh
@@ -2,13 +2,33 @@
 if [ ! -d ~/Escritorio/respaldo ]; then 
 	mkdir ~/Escritorio/respaldo 
 fi
+read -n1
 
-size = "`du ~/Escritorio/Cisco | cut -d '/' -f 1 | tail -l`"
-while [ $(($size)) -gt $(100000) ]
+size="`du ~/Escritorio/Cisco | cut -d '/' -f1 | tail -1`"
+echo $size
+
+read -n1
+
+while [ $size -gt $150000 ] 
 do
-    ls -t | tail -1 | rm -Ri 
+	cd ~/Escritorio/Cisco	
+	
+	archivo="`ls -t | cut -d '/' -f 1 | tail -1`"
+	
+	echo "Archivo mas viejo es: '$archivo'"
+    
+	read -n1
+    
+	rm -R -i $archivo
+
+	size="`du ~/Escritorio/Cisco | cut -d '/' -f1 | tail -1`"
+	echo $size
+
+	read -n1
 done
 cp -Rpa  ~/Escritorio/Cisco ~/Escritorio/respaldo 
+
+echo "Se hizo el respaldo en ~/Escritorio/respaldo"
 
 
 
