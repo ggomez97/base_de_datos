dce5b3a643e587cb05642a857f0d06d554b45548
ggomez97 <gabriel.mendaro97@gmail.com>
2018-07-23 02:44:39 -0300

    casf

--- script_backup.sh
+++ script_backup.sh
@@ -4,30 +4,24 @@ if [ ! -d ~/Escritorio/respaldo ]; then
 fi
 
 size = "`du ~/Escritorio/Cisco | cut -d '/' -f 1 | tail -l`"
-
-if [ $(($size)) -gt $(100000) ] then
-  echo("Entro")
-fi
-
 while [ $(($size)) -gt $(100000) ]
 do
-	rm -Ri | ls -t | tail -1
+    ls -t | tail -1 | rm -Ri 
 done
-
-#cp -Rpa  ~/Escritorio/Cisco ~/Escritorio/respaldo 
+cp -Rpa  ~/Escritorio/Cisco ~/Escritorio/respaldo 
 
 
 
 
 #!/bin/bash
-if [ ! -d ~/Escritorio/respaldo ]; then 
-	mkdir ~/Escritorio/respaldo 
-fi
+#if [ ! -d ~/Escritorio/respaldo ]; then 
+#	mkdir ~/Escritorio/respaldo 
+#fi
 
-size="`du ~/Escritorio/Cisco | cut -d '/' -f1 | tail -1`"
-echo $size
-if [ $size -gt 100000 ] then
-  echo("Entro")
-fi
+#size="`du ~/Escritorio/Cisco | cut -d '/' -f1 | tail -1`"
+#echo $size
+#if [ $size -gt 100000 ] then
+#  echo("Entro")
+#fi
 
 
